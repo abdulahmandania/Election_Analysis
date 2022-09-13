af48b4916c9681abcaed5b9c80ab81e7b8e0833c
Dania Abdulrahman <abdulrahmandania@gmail.com>
2022-09-13 15:04:21 -0500

    Adding PyPoll.py file.

---
+++ date_time_example.py
@@ -0,0 +1,6 @@
+#import datetime class from datetime module
+import datetime
+#Use the now() attribute on datetime class to get the present time. 
+now = datetime.datetime.now()
+#print present time
+print("The time right now is", now)
