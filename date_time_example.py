c2335b68dcd5b2030f1bfce9a358bd2f0b044cef
Dania Abdulrahman <abdulrahmandania@gmail.com>
2022-09-15 01:17:00 -0500

    Adding the election analysis.

--- date_time_example.py
+++
@@ -1,6 +0,0 @@
-#import datetime class from datetime module
-import datetime
-#Use the now() attribute on datetime class to get the present time. 
-now = datetime.datetime.now()
-#print present time
-print("The time right now is", now)
